ef882fba9e844bda256357b7564c29d84c585632
ReneDev <renedevtj@gmail.com>
2020-05-21 20:35:43 -0700

    Implemented View Gigs I'm attending

---
+++ GigHub/GigHub/Controllers/AttendancesController.cs
@@ -0,0 +1,44 @@
+﻿using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using GigHub.Dtos;
+
+namespace GigHub.Controllers
+{
+    //[Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto dto)
+        {
+            var userId = User.Identity.GetUserId();  //"10077b95-20c2-4e03-9b83-9a3ce55175cf";  // User.Identity.GetUserId();
+
+
+            if (_context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == dto.GigId))
+                return BadRequest("The attendance already exists.");
+
+            var attendance = new Attendance
+            {
+                GigId = dto.GigId,
+                AttendeeId = userId
+            };
+
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
